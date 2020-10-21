f88735941f5f638585f084d669875388ec728a53
Paul R. Hibbing <prhibbing@cmh.edu>
2020-10-21 09:44:12 -0500

    Initialize documentation

---
+++ R/SBprofiles.R
@@ -0,0 +1,10 @@
+if(getRversion() >= "2.15.1") utils::globalVariables(c(
+  "."
+))
+
+#' Learn about and use sedentary behavior profiles
+#'
+#' @importFrom magrittr %>% %T>% %<>% %$%
+#' @docType package
+#' @name SBprofiles
+NULL
