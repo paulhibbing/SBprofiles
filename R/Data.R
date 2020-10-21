1571678a09d36bc3fdd5fff54c7e2da4127e9fc4
Paul R. Hibbing <prhibbing@cmh.edu>
2020-10-21 10:33:20 -0500

    Bring in package data

---
+++ R/Data.R
@@ -0,0 +1,6 @@
+#' Data for use in code examples throughout the \code{SBprofiles} package
+#'
+#' This is a minute-by-minute data set for one participant in the National Health
+#' and Nutrition Examination Survey (NHANES). For details, see the
+#' \href{https://wwwn.cdc.gov/Nchs/Nhanes/2003-2004/PAXRAW_C.htm#Codebook}{NHANES documentation}.
+"example_data"
---
+++ data-raw/use_data.R
@@ -0,0 +1,17 @@
+rm(list = ls())
+library(magrittr)
+
+example_data <- readRDS("data-raw/example.rds")
+usethis::use_data(example_data)
+
+# list.files("data-raw", "^[ft]", full.names = TRUE) %>% dput
+
+c(
+  "data-raw/tree_1s.rds",
+  "data-raw/tree.rds",
+  "data-raw/forest_1s.rds",
+  "data-raw/forest.rds"
+) %>%
+lapply(readRDS) %>%
+stats::setNames(c("tree1", "tree5", "forest1", "forest5")) %>%
+usethis::use_data(internal = TRUE)
