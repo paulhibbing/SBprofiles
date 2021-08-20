71899599c8ba603d82d071a20a77d666a8300855
Paul R. Hibbing <paulhibbing@gmail.com>
2021-08-20 12:42:42 -0500

    Update internal data

--- data-raw/use_data.R
+++ data-raw/use_data.R
@@ -1,17 +1,43 @@
-rm(list = ls())
-library(magrittr)
+# Setup -------------------------------------------------------------------
 
-example_data <- readRDS("data-raw/example.rds")
-usethis::use_data(example_data)
+  rm(list = ls())
+  library(magrittr)
 
-# list.files("data-raw", "^[ft]", full.names = TRUE) %>% dput
+  rstudioapi::getActiveDocumentContext()$path %>%
+  dirname(.) %>%
+  dirname(.) %>%
+  setwd(.)
 
-c(
-  "data-raw/tree_1s.rds",
-  "data-raw/tree.rds",
-  "data-raw/forest_1s.rds",
-  "data-raw/forest.rds"
-) %>%
-lapply(readRDS) %>%
-stats::setNames(c("tree1", "tree5", "forest1", "forest5")) %>%
-usethis::use_data(internal = TRUE, overwrite = TRUE)
+# Example data ------------------------------------------------------------
+
+  update_example_data <- FALSE
+
+  if (file.exists("data/example_data.rda")) {
+    load("data/example_data.rda")
+    if (!isTRUE(all.equal(
+      readRDS("data-raw/example.rds"),
+      example_data
+    ))) message(
+      "`example_data` has changed -- if you wish to update,",
+      "\n uncomment lines 30-31",
+      " (In RStudio for Windows, highlight and ctrl + shift + C)",
+      "\n and run them (In RStudio for Windows, ctrl + enter)"
+    ) else message(
+      "example_data has not changed -- no need to update"
+    )
+  }
+
+  # example_data <- readRDS("data-raw/example.rds")
+  # usethis::use_data(example_data, overwrite = TRUE)
+
+# Models ------------------------------------------------------------------
+
+  c(
+    "data-raw/tree_1min.rds",
+    "data-raw/tree.rds",
+    "data-raw/forest_1min.rds",
+    "data-raw/forest.rds"
+  ) %>%
+  lapply(readRDS) %>%
+  stats::setNames(c("tree1", "tree5", "forest1", "forest5")) %>%
+  usethis::use_data(internal = TRUE, overwrite = TRUE)
